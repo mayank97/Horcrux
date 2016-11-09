b74d15572360e2e0b0e1b836636ca5d4e725e7a2
mayank97 <mayank.mk18@gmail.com>
2016-11-09 13:24:38 +0530

    Add files via upload

--- DBLPObject.java
+++ DBLPObject.java
@@ -2,15 +2,37 @@ package dblp;
 
 public class DBLPObject 
 {
-	String title;
-String gettitle(){return title;}
-@Override
-public String toString(){
-	return this.title;
-}
-void settitle(String x){
-	title=x;
-}
+	protected String mdate;
+	protected String key;
+	protected String author;
+	protected String title;
+	protected String url;
+	protected int year;
+	
+	public String getMDate()
+	{
+		return this.mdate;
+	}
+	public String getKey()
+	{
+		return this.key;
+	}
+	public String getAuthor()
+	{
+		return this.author;
+	}
+	public String getTitle()
+	{
+		return this.title;
+	}
+	public String getURL()
+	{
+		return this.url;
+	}
+	public int getYear()
+	{
+		return this.year;
+	}
 	
 	
 }
