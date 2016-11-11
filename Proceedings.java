58d16ea0d13998608c38eff88964b356a9a160be
mayank97 <mayank.mk18@gmail.com>
2016-11-11 14:59:41 +0530

    Add files via upload

--- Proceedings.java
+++ Proceedings.java
@@ -9,36 +9,5 @@ public class Proceedings extends DBLPObject
 	private String booktitle;
 	private String series;
 	private String publisher;
-	
-	public String getEditor()
-	{
-		return this.editor;
-	}
-	public String getVolume()
-	{
-		return this.volume;
-	}
-	public String getEE()
-	{
-		return this.ee;
-	}
-	public String getISBN()
-	{
-		return this.isbn;
-	}
-	public String getBookTitle()
-	{
-		return this.booktitle;
-	}
-	public String getSeries()
-	{
-		return this.series;
-	}
-	public String getPublisher()
-	{
-		return this.publisher;
-	}
-	
-
-	
+		
 }
