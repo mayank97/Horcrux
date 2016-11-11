ca1c27b211c868b7f1ab301e0074c9c983a3ede1
mayank97 <mayank.mk18@gmail.com>
2016-11-11 14:59:15 +0530

    Add files via upload

--- WebPage.java
+++ WebPage.java
@@ -4,12 +4,5 @@ public class WebPage extends DBLPObject
 {
 	private String note;
 	
-	public String getNote()
-	{
-		return this.note;
-	}
-	public void setNote(String note)
-	{
-		this.note = note;
-	}
+	
 }
